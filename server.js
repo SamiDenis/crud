9e42d703bba8d2385637a92753eef5d3c9cf4a33
Samantha Denis <sami.m.denis@gmail.com>
2022-06-07 10:20:56 -0400

    required dependencies

---
+++ server.js
@@ -0,0 +1,11 @@
+//////////////////////////
+//IMPORT OUR DEPENDENCIES
+//////////////////////////
+
+require('.dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const req = require('express/lib/request')
+const mongoose = require('mongoose')
+const path = require('path')
